79df7eae08836e84a223970073dee4fd70ba6175
Andrew Roth <a.roth.80@hotmail.com>
2024-12-20 02:10:53 -0800

    /login endpoint added

--- server.js
+++ server.js
@@ -101,6 +101,15 @@ app.get("/u/:id", (req, res) => {
   }
 });
 
+app.get("/login", (req, res) => {
+  const userId = req.cookies["user_id"];
+  const user = users[userId];
+  const templateVars = {
+    user,
+  };
+  res.render("login", templateVars);
+});
+
 app.post("/login", (req, res) => {
   const username = req.body.username;
   res.cookie("username", username);
